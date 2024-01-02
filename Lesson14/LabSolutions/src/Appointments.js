df6b35a2decae16ee2c5dffc93f32c786c963834
movarnell <movarnell@gmail.com>
2024-01-02 09:55:37 -0600

    Refactor Appointments and Nav components

--- Lesson14/LabSolutions/src/Appointments.js
+++ Lesson14/LabSolutions/src/Appointments.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+// In React 18+ you don't have to import React in each component.
 
 /** Note: Feel free to copy/paste this into your Appointments.js for easier visibility.
  *
@@ -15,41 +15,37 @@ import React, {Component} from 'react'
  *                    //return your elements here
  *                    //Each <tr> will also need a key={index} property
  *                    ))}
- * 
+ *
  * Step 3: Style it with css using the App.css file!
  *
  */
 
-function Appointments({customers}) { 
-  
-  
-   
-    return (
-      <table className='appointmentsTable'>
-        <thead>
-          <tr>
-            <td>First Name</td>
-            <td>Last Name</td>
-            <td>Appointment Time</td>
+function Appointments({ customers }) {
+  return (
+    <table className='appointmentsTable'>
+      <thead>
+        <tr>
+          <td>First Name</td>
+          <td>Last Name</td>
+          <td>Appointment Time</td>
+        </tr>
+      </thead>
+      <tbody>
+        {/* Below we map over the customers and create a new table row for each one */}
+        {/* Remember that each new child of this return must have a unique key, */}
+        {/*  thats why we assign index to the key value */}
+        {customers.map((customer, index) => (
+          // notice in our map we use customer in the singular and the array of customers
+          // is in the plural, this makes it easier to resolve what is going on when use the values.
+          <tr key={index}>
+            <td>{customer.firstName}</td>
+            <td>{customer.lastName}</td>
+            <td>{customer.appointmentTime}</td>
           </tr>
-        </thead>
-        <tbody>
-          {/* Below we map over the customers and create a new table row for each one */}
-          {/* Remember that each new child of this return must have a unique key, */}
-          {/*  thats why we assign index to the key value */}
-          {customers.map((customer, index) => (
-            // notice in our map we use customer in the singular and the array of customers
-            // is in the plural, this makes it easier to resolve what is going on when use the values.
-            <tr key={index}>
-              <td>{customer.firstName}</td>
-              <td>{customer.lastName}</td>
-              <td>{customer.appointmentTime}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
 export default Appointments;
